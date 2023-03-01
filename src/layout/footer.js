722d1b105105a0da1fd4e56313898df8ad597943
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2023-03-01 07:03:36 +0700

    upload

---
+++ src/layout/footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div>
+      <p
+        style={{
+          textAlign: 'center',
+          bottom: 0,
+          position: 'fixed',
+          width: '100%',
+          padding: '20px',
+        }}
+      >
+        Prepared By rivando al rasyid |{' '}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
